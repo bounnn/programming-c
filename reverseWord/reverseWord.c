866cac8684e8635f5665f564a645379511ebdfe2
Bounheuang SENKHAM <93262982+bounnn@users.noreply.github.com>
2021-12-24 21:57:53 +0700

    V2

--- reverseWord/reverseWord.c
+++ reverseWord/reverseWord.c
@@ -1,50 +1,35 @@
-#include <stdio.h>
- 
-void reverse(char* begin, char* end)
-{
-    char temp;
-    while (begin < end) {
-        temp = *begin;
-        *begin++ = *end;
-        *end-- = temp;
-    }
-}
- 
-// Function to reverse words*/
-void reverseWords(char* s)
-{
-    char* word_begin = s;
- 
-    // Word boundary
-    char* temp = s;
- 
-    // Reversing individual words as
-    // explained in the first step
-    while (*temp) {
-        temp++;
-        if (*temp == '\0') {
-            reverse(word_begin, temp - 1);
-        }
-        else if (*temp == ' ') {
-            reverse(word_begin, temp - 1);
-            word_begin = temp + 1;
-        }
-    }
- 
-    // Reverse the entire string
-    reverse(s, temp - 1);
-}
- 
-// Driver Code
+#include<stdio.h>
+
 int main()
 {
-    char s[] = "much so you miss I";
-    char a[] = "much so you miss I";
-    char* temp = s;
-    char* b = a;
-    reverse(a,a);
-    reverseWords(s);
-    printf("%s\n", s);
-    printf("%s\n", a);
+    char s1[30],s2[30];
+    int i=0,j=0;
+    printf("Enter the string: ");
+    gets(s1);
+
+    while (s1[i]!='\0')
+    {
+        i++;
+    }
+    while (i>0)
+    {
+        s2[j] = s1[--i];
+        ++j;
+    }
+    s2[j] = '\0';
+    for (i = 0; s2[i]!='\0'; i++)
+    {
+        if (s2[i+1] == ' ' || s2[i+1] == '\0')
+        {
+            for (j=i ; j >= 0 && s2[j] != ' '; j--)
+            {
+                printf("%c", s2[j]);
+            }
+            printf(" ");
+        }
+        
+    }
+    
+    
     return 0;
 }
